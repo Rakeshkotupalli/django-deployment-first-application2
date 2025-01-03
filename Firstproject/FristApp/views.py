434699b2d6133d1dd49cbaf147498740f7bc8995
Rakeshkotupalli <rkotupalli.com>
2025-01-03 12:15:22 +0530

    firstcommit

---
+++ Firstproject/FristApp/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse;
+
+# Create your views here.
+def display(request): #view-function
+	#ss ----> is html-data/code
+	ss = '''		
+			<center>
+				<h2 style="color:Blue;">
+					Hello RAKESH , Welcome to Django First-Project First-App
+				</h2>
+				<hr />
+			</center>
+		''';
+	
+	return HttpResponse(ss);
+
+
+def demo(request):   
+	print("mulitple-Requests-URLs same respose");
+	htmldata='''<center>
+		<h1>Welcome Demo User!!!</h1>
+		<hr />
+		<h2>This is Same-Output for diff-mulitple-Requests-URLs</h2>
+		<hr />
+		<h3>Have a Great Day...</h3>
+		</center>''';
+	return HttpResponse(htmldata);
+
+def homepage(request):
+    htmldata='''<center>
+        <h1>Welcome to DEFAULT Home-Page!!!</h1>
+        <hr />
+        <h2>Your Request Page is Not-Found...</h2>
+        <hr />
+        <h3>Plz try other URL or Links!!!</h3>
+    </center>''';
+    return HttpResponse(htmldata);
+    
+    re_path("^.*$",views.homepage),
+    #(from django.urls import re_path;)
+    
+
